34d5a7042f3433777069efc6c37ff3a7c6487116
lezhnina-anna <Ankemova@gmail.com>
2022-05-01 13:39:40 +0300

    Шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import {render} from './render';
 import UserView from './view/user-view';
 import CardListPresenter from './presenter/card-list-presenter';
-import MovieModel from './model/movie-model';
-import {getMoviesData} from "./data";
+import {getMoviesData} from './data';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = document.querySelector('.header');
